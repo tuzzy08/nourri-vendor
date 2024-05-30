8a677959ab8bcf6e0bc55cd6b7ebfd8a4b6962e6
xavier <tuzzy08@gmail.com>
2024-05-30 02:40:44 +0100

    Chore: Created order total display

--- components/OrderBox/index.tsx
+++ components/OrderBox/index.tsx
@@ -6,33 +6,85 @@ export default function OrderBox() {
 	return (
 		<View style={styles.container}>
 			<View style={styles.totalBox}>
-				<Text>Total</Text>
+				<Text style={[styles.totalText, { color: '#FDB72B' }]}>
+					Total Orders
+				</Text>
+				<Text style={[styles.totalFigure, { color: '#C70039' }]}>115</Text>
+				<Text style={[styles.totalText, { color: '#4D6D00' }]}>N20000</Text>
 			</View>
-			<View>
+			<View style={{ flexDirection: 'row', gap: 8 }}>
 				<View style={styles.todayBox}>
-					<Text>Today</Text>
+					<Text style={styles.totalText}>Today</Text>
+					<Text style={styles.totalFigure}>115</Text>
+					<Text style={styles.totalText}>N20000</Text>
 				</View>
 				<View style={styles.yesterdayBox}>
-					<Text>Yesterday</Text>
+					<Text style={styles.totalText}>Yesterday</Text>
+					<Text style={styles.totalFigure}>115</Text>
+					<Text style={styles.totalText}>N20000</Text>
 				</View>
 				<View style={styles.thisWeekBox}>
-					<Text>This Week</Text>
+					<Text style={styles.totalText}>This Week</Text>
+					<Text style={styles.totalFigure}>115</Text>
+					<Text style={styles.totalText}>N20000</Text>
 				</View>
 				<View style={styles.thisMonthBox}>
-					<Text>This Month</Text>
+					<Text style={styles.totalText}>This Month</Text>
+					<Text style={styles.totalFigure}>115</Text>
+					<Text style={styles.totalText}>N20000</Text>
 				</View>
 			</View>
-
-			<Text>OrderBox</Text>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {},
-	totalBox: {},
-	todayBox: {},
-	yesterdayBox: {},
-	thisWeekBox: {},
-	thisMonthBox: {},
+	container: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 15,
+	},
+	totalBox: {
+		width: 120,
+		height: 120,
+		// backgroundColor: 'red',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 5,
+	},
+	totalFigure: { fontSize: 20, fontWeight: 'bold' },
+	totalPrice: { fontSize: 1 },
+	totalText: { fontSize: 12, fontWeight: '500' },
+	todayBox: {
+		width: 80,
+		height: 90,
+		backgroundColor: '#C70039',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 5,
+	},
+	yesterdayBox: {
+		width: 80,
+		height: 90,
+		backgroundColor: '#84A100',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 5,
+	},
+	thisWeekBox: {
+		width: 80,
+		height: 90,
+		backgroundColor: '#4D6D00',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 5,
+	},
+	thisMonthBox: {
+		width: 80,
+		height: 90,
+		backgroundColor: '#FDB72B',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderRadius: 5,
+	},
 });
